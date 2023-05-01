6d50fbb723d4fa9b2a9d31587b6a841247de33f1
bwilkins96 <ben.jr.speed@gmail.com>
2023-05-01 14:11:46 -0400

    get change and valid move

--- knights_tour.py
+++ knights_tour.py
@@ -7,16 +7,24 @@
 from graph import Graph
 
 class KnightsTour:
-    def __init__(self):
-        self._board = self._setup_board()
-        self._visited = set()
+    def __init__(self, dimensions=(8,8)):
+        self._board = self._setup_board(dimensions)
 
-    def _setup_board(self):
+    def get_change(self, start, end):
+        row_change = abs(start[0] - end[0])
+        col_change = abs(start[1] - end[1])
+        return (row_change, col_change)
+
+    def valid_move(self, start, end):
+        change = self.get_change(start, end)
+        return change == (1, 2) or change == (2, 1)
+
+    def _setup_board(self, dimensions):
         board = Graph()
 
-        # Setup vertices for 8x8 chess board in the form of (row, column) 
-        for i in range(1, 9):
-            for j in range(1, 9):
+        # Setup vertices for chess board in the form of (row, column) 
+        for i in range(1, dimensions[0]+1):
+            for j in range(1, dimensions[1]+1):
                 board.insert_vertex((i, j))
 
         return board
