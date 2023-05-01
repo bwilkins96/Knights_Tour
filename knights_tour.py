36ef9c5545196f4b17046ad74d93a52369c1f7b3
bwilkins96 <ben.jr.speed@gmail.com>
2023-05-01 13:29:56 -0400

    setup vertices for graph

--- knights_tour.py
+++ knights_tour.py
@@ -3,3 +3,23 @@
 # Benjamin Wilkins
 
 # Knight's tour
+
+from graph import Graph
+
+class KnightsTour:
+    def __init__(self):
+        self._board = self._setup_board()
+        self._visited = set()
+
+    def _setup_board(self):
+        board = Graph()
+
+        # Setup vertices for 8x8 chess board in the form of (row, column) 
+        for i in range(1, 9):
+            for j in range(1, 9):
+                board.insert_vertex((i, j))
+
+        return board
+    
+if __name__ == '__main__':
+    print(KnightsTour()._board.vertices())
